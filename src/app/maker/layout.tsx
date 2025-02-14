89cfff4370327608a51157929541a0bcfac1f705
Elias Mollericona <omglasvi@gmail.com>
2025-02-14 00:13:20 -0400

    fix: fixing for the project

---
+++ src/app/maker/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "@/components";
+
+export default function ShopLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <main className="min-h-screen">
+
+      <Sidebar />
+      <div className="px-0 sm:px-10">
+        {children}
+      </div>
+      
+    </main>
+  );
+}
---
+++ src/app/maker/page.tsx
@@ -0,0 +1,11 @@
+import { MakeGrid } from "@/components";
+
+
+export default function Home(){
+
+    return(
+        <>
+            <MakeGrid />
+        </>
+    )
+}
